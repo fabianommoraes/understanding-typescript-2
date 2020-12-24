92b1254ee09dceee2f93f62b1691d3567a1ee01f
fabianommoraes <fabiano.m.moraes@gmail.com>
2020-12-24 12:09:16 -0300

    Adding Add Todo base component

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 // type TodoListProps = {
 //   items: { id: string; text: string }[];
 // };
 
+const inputId = 'todo-text';
+
 const NewTodo: React.FC = (props) => {
-  return <form></form>;
+  const textInputRef = useRef<HTMLInputElement>(null);
+
+  const todoSubmitHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+    const enteredText = textInputRef.current!.value;
+    console.log(enteredText);
+  };
+
+  return (
+    <form onSubmit={todoSubmitHandler}>
+      <div>
+        <label htmlFor={inputId}>Todo Text</label>
+        <input type="text" id={inputId} ref={textInputRef} />
+      </div>
+      <button type="submit">ADD TODO</button>
+    </form>
+  );
 };
 
 export default NewTodo;
