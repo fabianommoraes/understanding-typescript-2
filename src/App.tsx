ea3f524b8bfb09487a40f44c28593820f10fcaf3
fabianommoraes <fabiano.m.moraes@gmail.com>
2020-12-24 12:01:53 -0300

    Simple todo list component with props and types

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import TodoList from './components/TodoList';
+import NewTodo from './components/NewTodo';
 
-function App() {
+const App: React.FC = () => {
+  const todos = [{ id: 't1', text: 'Finish the course' }];
+
   return (
-    <div className="App">
-      <h1>TEST</h1>
+    <div>
+      <NewTodo />
+      <TodoList items={todos} />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/NewTodo.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// type TodoListProps = {
+//   items: { id: string; text: string }[];
+// };
+
+const NewTodo: React.FC = (props) => {
+  return <form></form>;
+};
+
+export default NewTodo;
